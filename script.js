83b35f338586290267a01b09d918637ee50b5f80
Hassan Fares <abdallahffh@gmail.com>
2022-10-18 17:16:44 -0400

    Add script and polish
    Just the remaining audio left, still, we have planned for adding a
    dropdown menu for selecting different styles of sound.

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const tiles = Array.from(document.querySelectorAll('.tile'));
+tiles.forEach(tile => tile.addEventListener('transitionend', removeTransition));
+
+const subtiles = Array.from(document.querySelectorAll('.sub-tile'));
+subtiles.forEach(subTile => subTile.addEventListener('transitionend', removeTransition));
+
 window.addEventListener("keydown", (e) => {
     const audio = document.querySelector(`audio[key="${e.key}"]`)
     const key = document.querySelector(`div[key="${e.key}"]`);
@@ -7,3 +13,8 @@ window.addEventListener("keydown", (e) => {
     audio.currentTime = 0;
     audio.play();
 })
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+  }
